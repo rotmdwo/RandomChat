6117eed8e2e5279c963002e5b07b673c1c22e98e
rotmdwo <rotmdehf@naver.com>
2022-07-24 17:28:36 +0900

    상대방이 메시지 보냈을 때 화면에 보여주는 콜백 추가, 메시지 형식 String -> Json 변경

--- app/src/main/java/com/sungjae/randomchat/ChatroomActivity.kt
+++ app/src/main/java/com/sungjae/randomchat/ChatroomActivity.kt
@@ -2,18 +2,20 @@ package com.sungjae.randomchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.ScrollView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
-import org.eclipse.paho.client.mqttv3.MqttClient
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.*
+import org.json.JSONObject
 
 class ChatroomActivity : AppCompatActivity() {
     private lateinit var mqttClient: MqttClient
+    private lateinit var llChat: LinearLayout
+    private lateinit var clientId: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +25,7 @@ class ChatroomActivity : AppCompatActivity() {
 
         val btnSend = findViewById<AppCompatButton>(R.id.btnSend)
         val etMessage = findViewById<AppCompatEditText>(R.id.etMessage)
-        val llChat = findViewById<LinearLayout>(R.id.llChat)
+        llChat = findViewById(R.id.llChat)
         val svChat = findViewById<ScrollView>(R.id.svChat)
 
         btnSend.setOnClickListener {
@@ -34,7 +36,7 @@ class ChatroomActivity : AppCompatActivity() {
 
                 // 내 메시지 버블 삽입
                 val view = LayoutInflater.from(this).inflate(R.layout.my_chat, null)
-                val textView = view.findViewById<AppCompatTextView>(R.id.tvMessage)
+                val textView = view.findViewById<AppCompatTextView>(R.id.tvMyMessage)
                 textView.text = message
                 llChat.addView(view)
 
@@ -50,12 +52,45 @@ class ChatroomActivity : AppCompatActivity() {
 
     private fun connect(): MqttClient {
         val url = "tcp://220.79.204.104:1883"
-        val mqttClient = MqttClient(url, MqttClient.generateClientId(), null) //persistence 파라미터 안 주면 에러남;;
+        if (!this::clientId.isInitialized) clientId = MqttClient.generateClientId()
+        val mqttClient = MqttClient(url, clientId, null) //persistence 파라미터 안 주면 에러남;;
         val connectOptions = MqttConnectOptions()
         connectOptions.userName = "test"
         connectOptions.password = "5534".toCharArray()
         mqttClient.connect(connectOptions)
+
+        // 수신 설정
         mqttClient.subscribe(getTopic())
+        mqttClient.setCallback(object : MqttCallback {
+            override fun connectionLost(cause: Throwable?) {
+                Log.d(application.packageName, "Connection Lost")
+                connect()
+            }
+
+            override fun messageArrived(topic: String?, message: MqttMessage?) {
+                if (topic == getTopic() && message != null) {
+                    val jsonObject = JSONObject(String(message.payload))
+                    val theirId = jsonObject.getString("id")
+                    val theirMessage = jsonObject.getString("content")
+
+                    if (theirId != clientId) {
+                        // 콜백함수 안에서 runOnUiThread 안 쓰면 UI 업데이트 안 됨
+                        runOnUiThread {
+                            // 상대 메시지 버블 삽입
+                            val view = LayoutInflater.from(this@ChatroomActivity).inflate(R.layout.their_chat, null)
+                            val textView = view.findViewById<AppCompatTextView>(R.id.tvTheirMessage)
+                            textView.text = theirMessage
+                            llChat.addView(view)
+                        }
+                    }
+                }
+            }
+
+            override fun deliveryComplete(token: IMqttDeliveryToken?) {
+                TODO("Not yet implemented")
+            }
+        })
+
         return mqttClient
     }
 
@@ -64,6 +99,7 @@ class ChatroomActivity : AppCompatActivity() {
     }
 
     private fun publish(mqttClient: MqttClient, topic: String, message: String) {
-        mqttClient.publish(topic, MqttMessage(message.toByteArray()))
+        val json = "{\"id\":\""+clientId+"\",\"content\":\""+message+"\"}"
+        mqttClient.publish(topic, MqttMessage(json.toByteArray()))
     }
 }
